dcdbd72dc1d7b75e5bde82ecec2a7e978140f4ea
zacky <E0284042@u.nus.edu>
2019-10-01 16:08:04 +0800

    Day 1 Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextComponent } from './components/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule
